7050a9d4d47cf610eb81c557c0df0dcdba27d070
AustinPardosi <gabrielpardosi26@gmail.com>
2024-03-10 20:10:40 +0700

    fix: add deploy endpoint

--- app/components/(pages)/Login.tsx
+++ app/components/(pages)/Login.tsx
@@ -1,19 +1,40 @@
-import { Text, TouchableOpacity, View, Image } from "react-native";
+import { Text, TouchableOpacity, View, Image, Linking } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import Expo from "expo";
+import { useEffect, useState } from "react";
 
 const LoginPage = () => {
   const signIn = async () => {
-    console.log("terklik");
+    // console.log("terklik");
     try {
-      const response = await fetch("http://10.0.2.2:8080/api/login", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log("masuk");
-      console.log("YEAY DATA", response);
+      const response = await fetch(
+        "https://ea2e-36-79-175-119.ngrok-free.app/api/login",
+        {
+          method: "post",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // console.log(response);
+
+      if (response.ok) {
+        // Pastikan responsnya ok sebelum melanjutkan
+        console.log(response);
+        const data = await response.json();
+        if (data && data.url) {
+          Linking.openURL(data.url);
+        }
+        // console.log("return");
+        // console.log(response);
+
+        // const jsonResponse = await response.json(); // Tambahkan `await` untuk menunggu promise selesai
+        // console.log("masuk");
+        // console.log(jsonResponse);
+        // console.log("YEAY DATA", jsonResponse, response.status); // jsonResponse sekarang berisi objek JSON yang diharapkan
+      } else {
+        console.log("Gagal mengambil data, status:", response.status);
+      }
     } catch (error) {
       console.log("ADA ERROR", error);
     }
