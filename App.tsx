d7ba3675f7b3fde2ed09b8e06b23b391116d268c
AustinPardosi <gabrielpardosi26@gmail.com>
2024-03-16 12:39:13 +0700

    fix: removing expo -left

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import ChatroomList from "./app/components/(pages)/ChatroomList";
+import LoginPage from "./app/components/(pages)/Login";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+export type RootStackParamList = {
+  ChatroomList: undefined;
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const checkLoginStatus = async () => {
+    // Try to login
+    setLoggedIn(false);
+  };
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {isLoggedIn ? (
+          <Stack.Screen name="ChatroomList" component={ChatroomList} options={{ headerShown: false }} />
+        ) : (
+          <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false }} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
